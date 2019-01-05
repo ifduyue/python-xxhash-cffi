182a6d6d744f372c4392a80d641098407ec4ee9b
Yue Du <ifduyue@gmail.com>
2019-01-05 18:16:06 +0800

    Remove nose

--- setup.py
+++ setup.py
@@ -26,8 +26,7 @@ setup(
     install_requires = ['cffi'],
     setup_requires = ['cffi'],
     cffi_modules = ['ffibuild.py:ffi'],
-    test_suite='nose.collector',
-    tests_require=['nose>1.3.0'],
+    test_suite='tests',
     classifiers=[
         'Development Status :: 5 - Production/Stable',
         'License :: OSI Approved :: BSD License',
